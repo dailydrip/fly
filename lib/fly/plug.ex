d18e641c869bed19474fa5763f6f6ae887be7814
Josh Adams <josh.rubyist@gmail.com>
2016-12-18 03:38:54 -0600

    Make plug use cache

--- lib/fly/plug.ex
+++ lib/fly/plug.ex
@@ -10,6 +10,7 @@ defmodule Fly.Plug do
     try do
       config_atom = String.to_existing_atom(config_atom_string)
       conn = fetch_query_params(conn)
+      cache_key = {config_atom, conn.query_params}
       url = conn.query_params["url"]
       file = get(url)
 
@@ -19,7 +20,7 @@ defmodule Fly.Plug do
         |> Enum.map(fn({k, v}) -> {String.to_existing_atom(k), v} end)
         |> Map.new
 
-      result = Fly.run(config_atom, file, options)
+      result = Fly.run_cached(cache_key, config_atom, file, options)
       conn
         |> resp(200, result)
     rescue
